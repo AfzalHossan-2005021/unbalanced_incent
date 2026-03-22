9eeb0f1c3f73008dbe3458ce3298a4e50342dc4b
Afzal Hossan <afzalhossan2005021@gmail.com>
2026-03-23 04:07:53 +0600

    import issue solved.

--- tempoa_solver.py
+++ tempoa_solver.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy.sparse as sp
-from ot.unbalanced import fused_unbalanced_gromov_wasserstein
+from ot.gromov import fused_unbalanced_gromov_wasserstein
 
 from .tempoa_features import tempoa_temporal_cost
 from .tempoa_cpd import tempoa_prior_matrix, compute_diffeomorphic_masked_cost
